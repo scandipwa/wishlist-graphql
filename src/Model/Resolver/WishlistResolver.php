bfd097e76fc0b6d167f67e9f1160dae24313b116
mageprincess <anna.bicevska@gmail.com>
2019-07-16 16:01:12 +0300

    Fixing wishlist graphql error for missing model (#1)

---
+++ src/Model/Resolver/WishlistResolver.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * ScandiPWA - Progressive Web App for Magento
+ *
+ * Copyright © Scandiweb, Inc. All rights reserved.
+ * See LICENSE for license details.
+ *
+ * @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
+ * @package scandipwa/wishlist-graphql
+ * @link    https://github.com/scandipwa/wishlist-graphql
+ */
+
+declare(strict_types=1);
+
+namespace ScandiPWA\WishlistGraphQl\Model\Resolver;
+
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Wishlist\Model\ResourceModel\Wishlist as WishlistResourceModel;
+use Magento\Wishlist\Model\Wishlist;
+use Magento\Wishlist\Model\WishlistFactory;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
+use Magento\Framework\GraphQl\Exception\GraphQlAuthorizationException;
+
+/**
+ * Fetches the Wishlist data according to the GraphQL schema
+ */
+class WishlistResolver implements ResolverInterface
+{
+    /**
+     * @var WishlistResourceModel
+     */
+    private $wishlistResource;
+
+    /**
+     * @var WishlistFactory
+     */
+    private $wishlistFactory;
+
+    /**
+     * @param WishlistResourceModel $wishlistResource
+     * @param WishlistFactory $wishlistFactory
+     */
+    public function __construct(WishlistResourceModel $wishlistResource, WishlistFactory $wishlistFactory)
+    {
+        $this->wishlistResource = $wishlistResource;
+        $this->wishlistFactory = $wishlistFactory;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        $customerId = $context->getUserId();
+
+        /** @var Wishlist $wishlist */
+        $wishlist = $this->wishlistFactory->create();
+        $this->wishlistResource->load($wishlist, $customerId, 'customer_id');
+
+        if (null === $wishlist->getId()) {
+            return [
+                'model' => $wishlist,
+            ];
+        }
+
+        return [
+            'sharing_code' => $wishlist->getSharingCode(),
+            'updated_at' => $wishlist->getUpdatedAt(),
+            'items_count' => $wishlist->getItemsCount(),
+            'name' => $wishlist->getName(),
+            'model' => $wishlist,
+        ];
+    }
+}
