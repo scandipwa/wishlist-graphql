bf16280c489821fcea36d76dfd013979a469aa54
Rihards Abolins <denkfrt@inbox.lv>
2021-09-28 14:36:46 +0300

    [3408] Wishlist id support

---
+++ src/Model/Resolver/Wishlist/IdResolver.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * ScandiPWA - Progressive Web App for Magento
+ *
+ * Copyright © Scandiweb, Inc. All rights reserved.
+ * See LICENSE for license details.
+ *
+ * @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
+ * @package scandipwa/wishlist-graphql
+ * @link    https://github.com/scandipwa/wishlist-graphql
+ */
+
+declare(strict_types=1);
+
+namespace ScandiPWA\WishlistGraphQl\Model\Resolver\Wishlist;
+
+use Magento\Wishlist\Model\Wishlist;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+
+class IdResolver implements ResolverInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        /** @var Wishlist $wishlist */
+        $wishlist = $value['model'] ?? null;
+
+        if (!$wishlist) {
+            return null;
+        }
+
+        $id = $wishlist->getId();
+        return $id;
+    }
+}
